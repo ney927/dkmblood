944367ccd3bd7fc9f836da042ad9921de179f505
Khizar <134430081+khizarrm@users.noreply.github.com>
2023-10-03 18:12:20 -0400

    links

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ const PORT = process.env.PORT ||3000;
  
 // Server Setup
 app.listen(PORT,console.log(`Server started on port ${PORT}\nhttp://localhost:${PORT}/home.html\nhttp://localhost:${PORT}/login.html\nhttp://localhost:${PORT}/mission.html\nhttp://localhost:${PORT}/profile.html`));
+console.log(`http://localhost:${PORT}/searchDonors.html`);
+console.log(`http://localhost:${PORT}/viewUsers.html`);
 app.use(express.static('public'));
 app.use(express.json());
 const fs = require('fs')
@@ -21,7 +23,6 @@ const fs = require('fs')
 //to use POST forms
 const parseUrl = require('body-parser')
 const encodeUrl = parseUrl.urlencoded({ extended: false })
-
 app.get('/get-users', async function(request, response){
     let rawdata = fs.readFileSync('data.json');
     let data = JSON.parse(rawdata);
